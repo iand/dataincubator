0bb61ee9280f64f08bebb9721851494771c2d961
me@iandavis.com <me@iandavis.com@ae611706-192f-11de-b208-e5a9466b8568>
2009-04-21 14:43:19 +0000

    Added subdirectory for cache

--- trunk/oecd/htdocs/index.php
+++ trunk/oecd/htdocs/index.php
@@ -1,3 +1,4 @@
 <?php
 define('STORE_URI', 'http://api.talis.com/stores/iand-dev1');
+define('DI_DOMAIN', 'oecd');
 require_once(dirname(dirname(dirname(__FILE__))).DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'paget.php');
