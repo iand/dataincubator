1185b13b221555d30d076a234c66773e54892696
Ian Davis <nospam@iandavis.com>
2009-04-21 14:43:33 +0000

    Added subdirectory for cache

--- ol/htdocs/index.php
+++ ol/htdocs/index.php
@@ -1,3 +1,4 @@
 <?php
 define('STORE_URI', 'http://api.talis.com/stores/openlibrary');
+define('DI_DOMAIN', 'ol');
 require_once(dirname(dirname(dirname(__FILE__))).DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'paget.php');
