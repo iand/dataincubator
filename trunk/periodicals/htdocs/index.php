efb2c80afe58601399b3569e1f6b94bd8bdda20c
me@iandavis.com <me@iandavis.com@ae611706-192f-11de-b208-e5a9466b8568>
2009-04-24 15:43:22 +0000

    Added periodicals

---
+++ trunk/periodicals/htdocs/index.php
@@ -0,0 +1,4 @@
+<?php
+define('STORE_URI', 'http://api.talis.com/stores/periodicals');
+define('DI_DOMAIN', 'periodicals');
+require_once(dirname(dirname(dirname(__FILE__))).DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'paget.php');
