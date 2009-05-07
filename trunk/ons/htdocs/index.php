00548300134bae811a7ec2cdbcb2519297edea36
me@iandavis.com <me@iandavis.com@ae611706-192f-11de-b208-e5a9466b8568>
2009-05-07 10:32:07 +0000

    Switched to ons store

--- trunk/ons/htdocs/index.php
+++ trunk/ons/htdocs/index.php
@@ -3,6 +3,6 @@
 // it defines two constants: AUTH_USER and AUTH_PWD
 include_once(dirname(__FILE__).DIRECTORY_SEPARATOR.'auth.php'); 
 
-define('STORE_URI', 'http://api.talis.com/stores/iand-dev4');
+define('STORE_URI', 'http://api.talis.com/stores/ons');
 define('DI_DOMAIN', 'ons');
 require_once(dirname(dirname(dirname(__FILE__))).DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'paget2.php');
