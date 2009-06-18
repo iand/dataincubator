5e79bccd9593694fae702544a124cc210bb5d5be
K.J.W.Alexander <K.J.W.Alexander@ae611706-192f-11de-b208-e5a9466b8568>
2009-06-18 06:26:54 +0000

    changed index file

--- trunk/yahooweather/htdocs/index.php
+++ trunk/yahooweather/htdocs/index.php
@@ -2,4 +2,28 @@
 
 define('STORE_URI', 'http://api.talis.com/stores/kwijibo-dev2');
 define('DI_DOMAIN', 'yahooweather');
-require_once(dirname(dirname(dirname(__FILE__))).DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'paget2.php');
+$requestUri = $_SERVER['REQUEST_URI'];
+if(preg_match('%/forecast/([A-Z]+\d+)%', $requestUri, $m)){
+	$xml = file_get_contents('http://weather.yahooapis.com/forecastrss?p='.$m[1]);
+	$dom = new DomDocument();
+	$dom->loadXML($xml);
+	$xsldom = new DOMDocument();
+	$xsldom->loadXML(file_get_contents('weather.xsl'));
+	$XslT = new XSLTProcesor();
+	$XslT->importStylesheet($xsldom);
+	$rdfxml = $XslT->transformToXML($dom);
+	header("content-type: application/rdf+xml");
+	echo $rdfxml;
+	die;
+} else {
+	echo "request URIs should be of the form /forecast/{yahoo area code}";
+}
+// if(preg_match('%/now%',$requestUri)){ // now resource uri
+// 	//redirect to current date stamped forecast uri
+// } else if(){ // dated resource uri
+// 	//redirect to
+// } else if() { //document uri
+// 	
+// }
+//require_once(dirname(dirname(dirname(__FILE__))).DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'paget2.php');
+?>
