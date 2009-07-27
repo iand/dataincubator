541eda2e72d1dca8f89e1fa7db235a4830afc2b1
me@iandavis.com <me@iandavis.com@ae611706-192f-11de-b208-e5a9466b8568>
2009-07-27 15:21:12 +0000

    Added a generic subdomain handler

--- trunk/generic/htdocs/index.php
+++ trunk/generic/htdocs/index.php
@@ -3,7 +3,7 @@
 // it defines two constants: AUTH_USER and AUTH_PWD
 //include_once(dirname(__FILE__).DIRECTORY_SEPARATOR.'auth.php'); 
 
-if (preg_match('^([a-zA-Z\-]+)\.dataincubator\.org', $_SERVER["HTTP_HOST"], $m)) {
+if (preg_match('~^([a-z\-]+)\.dataincubator\.org~i', $_SERVER["HTTP_HOST"], $m)) {
   define('DI_DOMAIN', $m[1]);
   define('STORE_URI', 'http://api.talis.com/stores/' . DI_DOMAIN);
   require_once(dirname(dirname(dirname(__FILE__))).DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'paget2.php');
