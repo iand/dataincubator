ebe9fc0daa5f8f83412d2223d85d35267f985f1d
Ian Davis <nospam@iandavis.com>
2009-07-27 15:18:23 +0000

    Added a generic subdomain handler

---
+++ generic/htdocs/index.php
@@ -0,0 +1,15 @@
+<?php
+// this file is not in SVN but contains the store credentials
+// it defines two constants: AUTH_USER and AUTH_PWD
+//include_once(dirname(__FILE__).DIRECTORY_SEPARATOR.'auth.php'); 
+
+if (preg_match('^([a-zA-Z\-]+)\.dataincubator\.org', _SERVER["HTTP_HOST"], $m)) {
+  define('DI_DOMAIN', $m[1]);
+  define('STORE_URI', 'http://api.talis.com/stores/' . DI_DOMAIN);
+  require_once(dirname(dirname(dirname(__FILE__))).DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'paget2.php');
+}
+else {
+  header("404 Not Found");  
+}
+
+
