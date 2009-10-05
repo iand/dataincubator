29def069ee93f7c7103f84622604155c61f4ab08
Ian Davis <nospam@iandavis.com>
2009-10-05 20:17:47 +0000

    Added sample datagovuk subdomain

---
+++ datagovuk/htdocs/index.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" dir="ltr">
+  <head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="shortcut icon" href="http://data.hmg.gov.uk/sites/default/files/hmg_favicon_0.png" type="image/x-icon" />
+    <title>Data | hmg.gov.uk/data</title>
+    <link type="text/css" rel="stylesheet" media="all" href="http://data.hmg.gov.uk/modules/aggregator/aggregator.css?y" />
+<link type="text/css" rel="stylesheet" media="all" href="http://data.hmg.gov.uk/modules/book/book.css?y" />
+<link type="text/css" rel="stylesheet" media="all" href="http://data.hmg.gov.uk/modules/node/node.css?y" />
+<link type="text/css" rel="stylesheet" media="all" href="http://data.hmg.gov.uk/modules/poll/poll.css?y" />
+<link type="text/css" rel="stylesheet" media="all" href="http://data.hmg.gov.uk/modules/system/defaults.css?y" />
+<link type="text/css" rel="stylesheet" media="all" href="http://data.hmg.gov.uk/modules/system/system.css?y" />
+<link type="text/css" rel="stylesheet" media="all" href="http://data.hmg.gov.uk/modules/system/system-menus.css?y" />
+<link type="text/css" rel="stylesheet" media="all" href="http://data.hmg.gov.uk/modules/user/user.css?y" />
+<link type="text/css" rel="stylesheet" media="all" href="http://data.hmg.gov.uk/sites/all/modules/cck/theme/content-module.css?y" />
+<link type="text/css" rel="stylesheet" media="all" href="http://data.hmg.gov.uk/sites/all/modules/date/date.css?y" />
+<link type="text/css" rel="stylesheet" media="all" href="http://data.hmg.gov.uk/sites/all/modules/date/date_popup/themes/datepicker.css?y" />
+<link type="text/css" rel="stylesheet" media="all" href="http://data.hmg.gov.uk/sites/all/modules/date/date_popup/themes/timeentry.css?y" />
+<link type="text/css" rel="stylesheet" media="all" href="http://data.hmg.gov.uk/sites/all/modules/filefield/filefield.css?y" />
+<link type="text/css" rel="stylesheet" media="all" href="http://data.hmg.gov.uk/sites/all/modules/fivestar/css/fivestar.css?y" />
+<link type="text/css" rel="stylesheet" media="all" href="http://data.hmg.gov.uk/sites/all/modules/lightbox2/css/lightbox.css?y" />
+<link type="text/css" rel="stylesheet" media="all" href="http://data.hmg.gov.uk/sites/all/modules/simplenews/simplenews.css?y" />
+<link type="text/css" rel="stylesheet" media="all" href="http://data.hmg.gov.uk/modules/forum/forum.css?y" />
+<link type="text/css" rel="stylesheet" media="all" href="http://data.hmg.gov.uk/sites/all/modules/cck/modules/fieldgroup/fieldgroup.css?y" />
+<link type="text/css" rel="stylesheet" media="all" href="http://data.hmg.gov.uk/sites/all/themes/hmg/css/master.css?y" />
+<link type="text/css" rel="stylesheet" media="print" href="http://data.hmg.gov.uk/sites/all/themes/hmg/print.css?y" />
+    <script type="text/javascript" src="http://data.hmg.gov.uk/misc/jquery.js?y"></script>
+<script type="text/javascript" src="http://data.hmg.gov.uk/misc/drupal.js?y"></script>
+<script type="text/javascript" src="http://data.hmg.gov.uk/sites/all/modules/fivestar/js/fivestar.js?y"></script>
+<script type="text/javascript" src="http://data.hmg.gov.uk/sites/all/modules/lightbox2/js/lightbox.js?y"></script>
+<script type="text/javascript" src="http://data.hmg.gov.uk/sites/all/themes/hmg/script.js?y"></script>
+<script type="text/javascript" src="http://data.hmg.gov.uk/sites/all/themes/hmg/js/jquery.ifixpng.js?y"></script>
+<script type="text/javascript" src="http://data.hmg.gov.uk/sites/all/themes/hmg/js/functions.js?y"></script>
+<script type="text/javascript" src="http://data.hmg.gov.uk/sites/all/themes/hmg/js/cufon-yui.js?y"></script>
+<script type="text/javascript" src="http://data.hmg.gov.uk/sites/all/themes/hmg/js/Helvetica_Neue_100.font.js?y"></script>
+<script type="text/javascript">
+<!--//--><![CDATA[//><!--
+jQuery.extend(Drupal.settings, { "basePath": "/", "fivestar": { "titleUser": "Your rating: ", "titleAverage": "Average: ", "feedbackSavingVote": "Saving your vote...", "feedbackVoteSaved": "Your vote has been saved.", "feedbackDeletingVote": "Deleting your vote...", "feedbackVoteDeleted": "Your vote has been deleted." }, "googleanalytics": { "trackOutgoing": 1, "trackMailto": 1, "trackDownload": 1, "trackDownloadExtensions": "7z|aac|avi|csv|doc|exe|flv|gif|gz|jpe?g|js|mp(3|4|e?g)|mov|pdf|phps|png|ppt|rar|sit|tar|torrent|txt|wma|wmv|xls|xml|zip" }, "lightbox2": { "rtl": 0, "file_path": "/(\\w\\w/)sites/default/files", "default_image": "/sites/all/modules/lightbox2/images/brokenimage.jpg", "border_size": 10, "font_color": "000", "box_color": "fff", "top_position": "", "overlay_opacity": 0.8, "overlay_color": "000", "disable_close_click": true, "resize_sequence": 0, "resize_speed": 400, "fade_in_speed": 400, "slide_down_speed": 600, "use_alt_layout": false, "disable_resize": false, "disable_zoom": false, "force_show_nav": false, "loop_items": false, "node_link_text": "View Image Details", "node_link_target": false, "image_count": "Image !current of !total", "video_count": "Video !current of !total", "page_count": "Page !current of !total", "lite_press_x_close": "press \x3ca href=\"#\" onclick=\"hideLightbox(); return FALSE;\"\x3e\x3ckbd\x3ex\x3c/kbd\x3e\x3c/a\x3e to close", "download_link_text": "", "enable_login": false, "enable_contact": false, "keys_close": "c x 27", "keys_previous": "p 37", "keys_next": "n 39", "keys_zoom": "z", "keys_play_pause": "32", "display_image_size": "", "image_node_sizes": "()", "trigger_lightbox_classes": "", "trigger_lightbox_group_classes": "", "trigger_slideshow_classes": "", "trigger_lightframe_classes": "", "trigger_lightframe_group_classes": "", "custom_class_handler": 0, "custom_trigger_classes": "", "disable_for_gallery_lists": true, "disable_for_acidfree_gallery_lists": true, "enable_acidfree_videos": true, "slideshow_interval": 5000, "slideshow_automatic_start": true, "slideshow_automatic_exit": true, "show_play_pause": true, "pause_on_next_click": false, "pause_on_previous_click": true, "loop_slides": false, "iframe_width": 600, "iframe_height": 400, "iframe_border": 1, "enable_video": false } });
+//--><!]]>
+</script>
+<script type="text/javascript">
+<!--//--><![CDATA[//><!--
+Cufon.replace('#page-header h1');
+//--><!]]>
+</script>
+    <!--[if IE 8]><link rel="stylesheet" type="text/css" href="http://data.hmg.gov.uk/sites/all/themes/hmg/css/ie8.css" /><![endif]--><!--[if IE 7]><link rel="stylesheet" type="text/css" href="http://data.hmg.gov.uk/sites/all/themes/hmg/css/ie7.css" /><![endif]--><!--[if IE 6]><link rel="stylesheet" type="text/css" href="http://data.hmg.gov.uk/sites/all/themes/hmg/css/ie6.css" /><![endif]-->  </head>
+<style type="text/css">
+tr, th { border: 0px; }
+</style>
+
+  <body class="not-front not-logged-in page-node node-type-page no-sidebars">
+
+  <div id="header-wrap">
+                <div id="header">
+                        <h1><a href="http://data.hmg.gov.uk/">hmg.gov.uk/data<span></span></a></h1>
+      <img src="http://data.hmg.gov.uk/sites/all/themes/hmg/images/HMGovernment-logo.gif" alt="Her Majesty's Government Logo" />        <img src="http://data.hmg.gov.uk/sites/all/themes/hmg/images/preview.png" alt="" id="preview" />
+                </div>
+        </div>
+
+  <div id="topbar-wrap">
+                <div id="topbar" class="section-data">
+                  <ul id="main-nav"><li class="menu-361 first"><a href="http://data.hmg.gov.uk/home" title="Opening up the conversation">Home</a></li>
+<li class="menu-364 active-trail active"><a href="http://data.hmg.gov.uk/data" title="Data" class="active">Data</a></li>
+<li class="menu-370"><a href="http://data.hmg.gov.uk/blog" title="Blog">Blog</a></li>
+<li class="menu-366"><a href="http://data.hmg.gov.uk/apps" title="Apps">Apps</a></li>
+<li class="menu-367"><a href="http://data.hmg.gov.uk/ideas" title="Ideas">Ideas</a></li>
+<li class="menu-368"><a href="http://data.hmg.gov.uk/resources" title="Resources">Resources</a></li>
+<li class="menu-369 last"><a href="http://data.hmg.gov.uk/about" title="About">About</a></li>
+</ul>                                   <div id="share">
+        
+<div id="block-apisearch-search" class="clear-block block block-apisearch  box search">
+
+  <h2>Search Data Feeds</h2>
+
+  <h2>Search Data</h2>
+<p>To list all datasets, search without any keywords.</p>
+<form action="/data/list" method="get">
+                                                <fieldset>
+                                                        <label for="data-search">Enter keyword(s)</label>
+                                                        <input name="keyword" id="data-search" type="text" value="" />
+                                                        <span class="notice">e.g. nhs, traffic, local authority, uk, XML</span>
+                                                        <input class="btn" id="data-search-submit" value="Search" type="submit" />
+                                                        <p>Powered by: <a href="http://www.ckan.net/">CKAN</a></p>
+                                                </fieldset>
+</form>
+<div class="info">
+</div>
+                                        </div>                  </div>
+                        </div>
+        </div>
+  
+  <div id="main-wrap">
+                <div id="main">
+
+                        <div id="mid-col">
+
+                <div id="internal-header">
+                
+                
+                
+<?php
+// CHANGE THESE TO POINT TO YOUR INSTALLATIONS OF MORIARTY AND ARC
+define('MORIARTY_DIR', '../../../lib/moriarty/');
+define('MORIARTY_ARC_DIR', '../../../lib/arc_2008_11_18/');
+
+$domain_map = array( 'datagovuk.dataincubator.org.local' => array('domain'=>'education.data.gov.uk', 'store'=>'govuk-education'));
+
+
+$this_host = $_SERVER["HTTP_HOST"];
+
+if (array_key_exists($this_host, $domain_map)) {
+  $uri = 'http://' . $domain_map[$this_host]['domain'] . $_SERVER["REQUEST_URI"];
+  $store_uri = 'http://api.talis.com/stores/' . $domain_map[$this_host]['store'];
+
+  require_once MORIARTY_DIR . 'moriarty.inc.php';
+  require_once MORIARTY_DIR . 'store.class.php';
+  require_once MORIARTY_DIR . 'simplegraph.class.php';
+
+  $store = new Store($store_uri);
+  $response = $store->describe($uri, 'lcbd', 'json');
+  if ($response->is_success()) {
+    $g = new SimpleGraph();
+    $g->add_labelling_property('http://education.data.gov.uk/ontology/school#establishmentName');
+    $g->from_json($response->body);
+    echo '<h1>' . htmlspecialchars($g->get_label($uri)) . '</h1>';
+  }
+  else {
+    echo '<p>Error: ' . htmlspecialchars($response->to_string()) . '</p>';
+  }
+  
+}
+
+
+?>                
+    </div>                          
+    <div class="plain">
+        
+<?php
+    if ($g) {
+      echo $g->to_html($uri);
+    }
+?>                                    
+
+    </div>
+                        
+  </div>
+  <script type="text/javascript" src="http://data.hmg.gov.uk/sites/all/modules/google_analytics/googleanalytics.js?y"></script>
+<script type="text/javascript">
+<!--//--><![CDATA[//><!--
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+//--><!]]>
+</script>
+<script type="text/javascript">
+<!--//--><![CDATA[//><!--
+try{var pageTracker = _gat._getTracker("UA-10855508-1");pageTracker._trackPageview();} catch(err) {}
+//--><!]]>
+</script>
+  </body>
+</html>
