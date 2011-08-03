7e96efbabcfe843be1c3a1a861345bac49936251
K.J.W.Alexander@gmail.com <K.J.W.Alexander@gmail.com@ae611706-192f-11de-b208-e5a9466b8568>
2011-08-03 10:05:37 +0000

    added metoffice

---
+++ metoffice/htdocs/index.php
@@ -0,0 +1,4 @@
+<?php
+define('STORE_URI', 'http://api.talis.com/stores/metoffice');
+define('DI_DOMAIN', 'metoffice');
+require_once(dirname(dirname(dirname(__FILE__))).DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'paget2.php');
